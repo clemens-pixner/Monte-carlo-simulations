90d355fa11852d08fbeef18cbd7acdd021f5ebc9
clemens-pixner <clemenspixner2.0@gmail.com>
2026-04-17 18:33:03 +0200

    feat: enhance revenue calculations and add weather demand logic in Inputs and Events classes

--- helicalc_montecarlo.py
+++ helicalc_montecarlo.py
@@ -10,37 +10,32 @@ class TriangularDistribution:
     
 class Inputs:
     def __init__(self):
-        self.flighthours = TriangularDistribution(20.00, 80.00, 45.00)
-
-        def weather():
-            r = random.random()
-
-            if r < 0.60: #60% normal weather
-                return 1.00
-            
-            elif r < 0.85: #25% bad weather
-                return 0.5
-            
-            else: #15% good weather
-                return 1.4
-        
+        self.flighthours = TriangularDistribution(20.00, 80.00, 45.00)  
         self.priceperminute = 32.00
         
-        def additional_revenue():
+        def additional_revenue(self):
             r = random.random()
 
             if r < 0.80: #80% no additional revenue
-                self.additional_revenue = 0.00
-            
+                return 0.00
             else: #20% additional revenue 
-                self.additional_revenue = TriangularDistribution(2000.00, 12000.00, 5000.00)
-            
-            return self.additional_revenue
+                return TriangularDistribution(2000.00, 12000.00, 5000.00).sample()
+        
+        def revenue(self):
+            flighthours_value = self.flighthours.sample()
+            base_revenue = flighthours_value * 60 * self.priceperminute
+            extra_revenue = self.additional_revenue()
+            return base_revenue + extra_revenue
         
 class VariableCosts:
     def __init__(self):
         self.doc = TriangularDistribution()
         self.overhaul_reserve = TriangularDistribution()
+
+    def total_variable_costs(self, flighthours):
+        doc_cost = self.doc.sample() * flighthours
+        overhaul_cost = self.overhaul_reserve.sample() * flighthours
+        return doc_cost, overhaul_cost
 
 class FixedCosts:
     def __init__(self):
@@ -49,34 +44,66 @@ class FixedCosts:
         self.salary = 5000.00
         self.admin = TriangularDistribution()
 
-        def other():
-            pass
+        def other(self):
+            r = random.random()
+            return TriangularDistribution().sample()
+        
+        def total_fixed_costs(self):
+            return(
+                self.aoc.sample()
+                + self.insurance.sample()
+                + (self.salary * 12)
+                + self.admin.sample()
+                + self.other()
+            )
 
 class Financing: 
     def __init__(self):
         self.purchase_price = None
         self.equity = None
-        self.loan = None
+        self.loan = self.purchase_price - self.equity
+        self.duration = None
         
-        def interest_rate():
+        def interest_rate(self):
             pass
 
-        self.duration = None
-        self.monthly_payment = None
+        def monthly_payment(self):
+            r = self.interest_rate() / 12
+            n = self.duration * 12
+            L = self.loan
 
+            return L * (r * (1 + r)**n) / ((1 + r)**n - 1)
 
 class Depreciation:
     def __init__(self):
         self.holding_period = None
-        self.monthly_depreciation = None
+        self.purchase_price = None
         self.residual_value = None
 
+    def monthly_depreciation(self):
+        return (self.purchase_price - self.residual_value) / (self.holding_period * 12)
 
-            
-                
-                
+class Events:
+    def technical_failure():
+        r = random.random()
 
+        if r < 0.85: #85% no additional maintenance
+            return 0.0
+        elif r < 0.97: #12% medium maintenance
+            return TriangularDistribution(5000.00, 50000.00, 15000.00).sample() 
+        elif r < 0.997: #2.7 high maintenance 
+            return TriangularDistribution(30000.00, 200000.00, 80000.00).sample()
+        else: #0.3% medium + high maintenance
+            medium = TriangularDistribution(5000.00, 50000.00, 15000.00).sample()
+            high = TriangularDistribution(30000.00, 200000.00, 80000.00).sample()
+            return medium + high
 
+    def weather_demand(self):
+        r = random.random()
 
-
-
+        if r < 0.60: #60% normal weather/demand
+            return 1.00
+        elif r < 0.85: #25% bad weather/demand
+            return 0.5
+        else: #15% good weather/demand
+            return 1.4
